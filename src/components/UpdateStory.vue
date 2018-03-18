38fc2fdb5d32ae9940c70248975042f0ff591bea
Carl Bednorz <carl@frnkn.com>
2018-03-18 21:30:05 +0100

    Form validation for input field implemented

--- src/components/UpdateStory.vue
+++ src/components/UpdateStory.vue
@@ -6,6 +6,9 @@
             <div class="columns">
                 <div class="column col-12">
                     <h1> Update Your User Story With ID {{ $route.params.storyId }}</h1>
+                    <div v-if="fbError" class="toast toast-error">
+                            {{fbErrorMsg}}
+                    </div>
                     <!--<UserStoryUpdateForm pwho="TEST WHO" pwhy="TEST WHY" pwhat="mfsanflkdsn" pacceptance_criteria="FDSAFK"></UserStoryUpdateForm>-->
                     <form @submit.prevent="updateStory">
                         <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
@@ -14,16 +17,31 @@
                         <div class="columns">
                         
                             <div class="column col-xs-12">
-                                <label class="form-label label-lg">As a </label> <input class="form-input input-lg"  v-model="storyUserOwned.who" name="who" type="text"  >
+                                <label class="form-label label-lg">As a<small>*</small> </label> 
+                                <input class="form-input input-lg" placeholder="Who" name="who" type="text" v-model="storyUserOwned.who" v-validate="'required'" :class="{'input': true, 'is-error': errors.has('who')}" />
+                                <p v-show="errors.has('who')" class="form-input-hint">{{ errors.first('who') }}</p>
+                            </div>
+                             <div class="column col-xs-12">
+                                <label class="form-label label-lg">I'd like to<small>*</small> </label> 
+                                <input class="form-input input-lg" placeholder="What" name="what" type="text" v-model="storyUserOwned.what" v-validate="'required'" :class="{'input': true, 'is-error': errors.has('what')}" />
+                                <p v-show="errors.has('what')" class="form-input-hint">{{ errors.first('what') }}</p>
                             </div>
                             <div class="column col-xs-12">
-                                <label class="form-label label-lg">I'd like to </label> <input class="form-input input-lg"   v-model="storyUserOwned.what" placeholder="What" name="what" type="text" >
-                            </div>
-                            <div class="column col-xs-12">
-                                <label class="form-label label-lg">so that </label> <input class="form-input input-lg"  v-model="storyUserOwned.why" placeholder="Why" name="why" type="text" >
+                                <label class="form-label label-lg">so that<small>*</small> </label> 
+                                <input class="form-input input-lg" placeholder="Why" name="why" type="text" v-model="storyUserOwned.why"  v-validate="'required'" :class="{'input': true, 'is-error': errors.has('why')}">
+                                <p v-show="errors.has('why')" class="form-input-hint">{{ errors.first('why') }}</p>
                             </div>
                         </div>
-                        
+                        <div class="columns">
+                            <div class="column col-xs-12">
+                                <label class="form label">Story Points:</label> 
+                                <input class="form-input" placeholder="Story Points" name="story_points" type="number" v-model="storyUserOwned.storyPoints">
+                            </div>
+                            <div class="column col-xs-12">
+                                <label class="form label">Business Value:</label> 
+                                <input class="form-input" placeholder="Business Value" name="business_value" type="number" v-model="storyUserOwned.businessValue">
+                            </div>
+                        </div>
                         <div class="acceptance-criterias-block form-group">
                             <label class="form-label">Acceptance Criterias: </label>
                             <textarea class="form-input" rows="10" placeholder="Enter Acceptance Criteria in Markdown here"   v-model="storyUserOwned.acceptance_criteria" name="acceptance_criteria"  />
@@ -47,6 +65,11 @@ import Header from './Header'
 let db = firebaseApp.database()
 let storiesRef = db.ref('stories')
 let storiesUserOwnedRef = db.ref('storiesUserOwned/')
+
+/**
+ * @todo: Form Validation
+ * @todo: business points and value
+ * */
 
 export default {
   name: 'UpdateStory',
@@ -82,35 +105,43 @@ export default {
   data () {
       return {
           storyId: null,
-          who: '',
-          what: '',
-          why: '',
-          acceptance_criteria: '', 
           user: {},
           story: {},
-          storyUserOwned: {}
+          storyUserOwned: {},
+          fbError: false,
+          fbErrorMsg: ''
       }
   },
   methods: {
       updateStory: function(){
-        console.log("UPDATE STORY", this.who, this.what, this.why, this.user)
-        
-        
-        //let storiesRef = storiesUserOwnedRef.child(this.user.uid).child(storyId)
-        console.log("Story User Owned", this.storyUserOwned)
-        let theStory = this.storyUserOwned
-        console.log("KEY", this.storyUserOwned['.key'])
-        let theKey = this.storyUserOwned['.key']
-        delete theStory['.key']
-        //Workaround for firebase key error
-        //delete this.storiesUserOwned['.key']
-        console.log("USER", this.user.uid)
-        //console.log("Story User Owned", this.storyUserOwned)
-        storiesUserOwnedRef.child(this.user.uid).child(theKey).update(theStory)
-        //Set who, what, why, acc_criteria, story points, business value
-        //console.log("STORIES REF", this.storyUserOwned)
+        this.$validator.validateAll().then((result) => {
+            if (result) {
+                console.log("Story User Owned", this.storyUserOwned)
+                let theStory = this.storyUserOwned
+                console.log("KEY", this.storyUserOwned['.key'])
+                let theKey = this.storyUserOwned['.key']
+                delete theStory['.key']
+                //Workaround for firebase key error
+                console.log("USER", this.user.uid)
+                storiesUserOwnedRef.child(this.user.uid).child(theKey).update(theStory)
 
-        this.$router.push({path: '/stories'})
+                // Reset Validation
+                this.$nextTick().then(() => {
+                    this.$validator.reset()
+                    this.errors.clear();
+                    this.fbError = false
+                    this.fbErrorMsg = ''
+
+                }) 
+                
+                this.$router.push({path: '/stories'})
+            }
+            else {
+                 console.log("NOT VALID") //@todo: Emit error message in UI from form validation in create form
+                this.fbErrorMsg = "The form is not valid! Please correct the errors!"
+                this.fbError = true
+            }
+        })
       }
   },
   mounted: function() {
